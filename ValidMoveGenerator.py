fe0222d68126da28eae7a89a66e7cd6f301ad3bb
Najaf <najaf1232075@gmail.com>
2023-08-12 02:54:27 +0530

    created validmoves generator for every piece except knight

--- ValidMoveGenerator.py
+++ ValidMoveGenerator.py
@@ -1,12 +1,239 @@
 def GenerateMove(piece,board_representation,cord):
     if piece.name=='r' or piece.name=='R':
-        rook(cord,board_representation)
-def pawn(cord):
-    ...
-def rook(cord,board_representation):
+        return rook(cord,board_representation)
+    elif piece.name=='p' or piece.name=='P':
+        return pawn(cord,board_representation)
+    elif piece.name == 'b' or piece.name == 'B':
+        return bishop(cord,board_representation)
+    elif piece.name == 'q' or piece.name == 'Q':
+        return queen(cord,board_representation)
+    elif piece.name == 'k' or piece.name == 'K':
+        return king(cord,board_representation)
+    
+
+def pawn(cord,board_representation):
     x = cord[0]
     y = cord[1]
     x = int(x/75)
     y = int(y/75)
     ValidMoves = []
+    opponents = []
+    isWhite = board_representation[y][x].isupper()
+    if isWhite:
+        y1=y-1
+        if y==6:
+            if board_representation[y1][x] == 0:
+                ValidMoves.append([x*75,y1*75])
+                if board_representation[y1-1][x] == 0:
+                    ValidMoves.append([x*75,(y1-1)*75])
+        else:
+            if board_representation[y1][x] == 0:
+                ValidMoves.append([x*75,y1*75])
+        if x+1<8 :
+            if board_representation[y1][x+1] != 0:
+                if board_representation[y1][x+1].isupper() !=  isWhite:
+                    opponents.append([(x+1)*75,y1*75])
+        if x-1>=0:
+            if board_representation[y1][x-1] != 0:
+                if board_representation[y1][x-1].isupper() != isWhite:
+                    opponents.append([(x-1)*75,y1*75])
+    else:
+        y1=y+1
+        if y==1:
+            if board_representation[y1][x] == 0:
+                ValidMoves.append([x*75,y1*75])
+                if board_representation[y1+1][x] == 0:
+                    ValidMoves.append([x*75,(y1+1)*75])
+        else:
+            if board_representation[y1][x] == 0:
+                ValidMoves.append([x*75,y1*75])
+        if x+1<8 :
+            if board_representation[y1][x+1] != 0:
+                if board_representation[y1][x+1].isupper() != isWhite:
+                    opponents.append([(x+1)*75,y1*75])
+        if x-1>=0:
+            if board_representation[y1][x-1] != 0:
+                if board_representation[y1][x-1].isupper() != isWhite:
+                    opponents.append([(x-1)*75,y1*75])
+        
+    return ValidMoves,opponents
+
+def rook(cord,board_representation):
+    x = cord[0]
+    y = cord[1]
+    x = int(x/75)
+    y = int(y/75)
+    isWhite = board_representation[y][x].isupper()
+    x1 = x+1
+    ValidMoves = []
+    opponents = []
+    while x1<8:
+        if board_representation[y][x1] == 0:
+            ValidMoves.append([x1*75,y*75])
+        elif board_representation[y][x1].isupper() !=  isWhite:
+            opponents.append([x1*75,y*75])
+            break
+        else:
+            break
+        x1+=1
     
+    x2=x-1
+    while x2 >= 0:
+        if board_representation[y][x2] == 0:
+            ValidMoves.append([x2*75,y*75])
+        elif board_representation[y][x2].isupper() != isWhite:
+            opponents.append([x2*75,y*75])
+            break
+        else:
+            break
+        x2-=1
+
+    y1=y+1
+    while y1<8:
+        if board_representation[y1][x] == 0:
+            ValidMoves.append([x*75,y1*75])
+        elif board_representation[y1][x].isupper() != isWhite:
+            opponents.append([x*75,y1*75])
+            break
+        else:
+            break
+        y1+=1
+
+    y2=y-1
+    while y2>=0:
+        if board_representation[y2][x] == 0:
+            ValidMoves.append([x*75,y2*75])
+        elif board_representation[y2][x].isupper() != isWhite:
+            opponents.append([x*75,y2*75])
+            break
+        else:
+            break
+        y2-=1
+    
+    return ValidMoves,opponents
+
+def bishop(cord,board_representation):
+    x = cord[0]
+    y = cord[1]
+    x = int(x/75)
+    y = int(y/75)
+    ValidMoves = []
+    opponents = []
+    isWhite = board_representation[y][x].isupper()
+    y1 = y - 1 
+    y2 = y + 1
+    x1 = x + 1
+    x2 = x - 1
+
+    while y1>=0 and x1 < 8:
+        if board_representation[y1][x1] == 0:
+            ValidMoves.append([x1*75,y1*75])
+        elif board_representation[y1][x1].isupper() != isWhite:
+            opponents.append([x1*75,y1*75])
+            break
+        else:
+            break
+        x1+=1
+        y1-=1
+    y1 = y - 1
+    while y1 >= 0 and x2 >= 0:
+        if board_representation[y1][x2] == 0:
+            ValidMoves.append([x2*75,y1*75])
+        elif board_representation[y1][x2].isupper() != isWhite:
+            opponents.append([x2*75,y1*75])
+            break
+        else:
+            break
+        x2-=1
+        y1-=1
+
+    x1 = x + 1
+    x2 = x - 1
+
+    while y2 < 8 and x1 < 8:
+
+        if board_representation[y2][x1] == 0:
+            ValidMoves.append([x1*75,y2*75])
+        elif board_representation[y2][x1].isupper() != isWhite:
+            opponents.append([x1*75,y2*75])
+            break
+        else:
+            break
+        x1+=1
+        y2+=1
+    
+    y2 = y + 1
+    while y2 < 8 and x2 >= 0:
+        if board_representation[y2][x2] == 0:
+            ValidMoves.append([x2*75,y2*75])
+        elif board_representation[y2][x2].isupper() != isWhite:
+            opponents.append([x2*75,y2*75])
+            break
+        else:
+            break
+        x2-=1
+        y2+=1
+
+    return ValidMoves,opponents
+
+def queen(cord,board_representation):
+    moves_like_rook = rook(cord,board_representation)
+    moves_like_bishop = bishop(cord,board_representation)
+    ValidMoves = moves_like_rook[0]+moves_like_bishop[0]
+    opponents = moves_like_rook[1]+moves_like_bishop[1]
+    return ValidMoves,opponents
+
+def king(cord,board_representation):
+    x = cord[0]
+    y = cord[1]
+    x = int(x/75)
+    y = int(y/75)
+    ValidMoves = []
+    opponents = []
+    isWhite = board_representation[y][x].isupper()
+    if x+1<8:
+        if board_representation[y][x+1] == 0:
+            ValidMoves.append([(x+1)*75,y*75])
+        elif board_representation[y][x+1].isupper() != isWhite:
+            opponents.append([(x+1)*75,y*75])
+    if x-1>=0:
+        if board_representation[y][x-1] == 0:
+            ValidMoves.append([(x-1)*75,y*75])
+        elif board_representation[y][x-1].isupper() != isWhite:
+            opponents.append([(x-1)*75,y*75])
+    if y+1<8:
+        if board_representation[y+1][x] == 0:
+            ValidMoves.append([x*75,(y+1)*75])
+        elif board_representation[y+1][x].isupper() != isWhite:
+            opponents.append([x*75,(y+1)*75])
+    if y-1>=0:
+        if board_representation[y-1][x] == 0:
+            ValidMoves.append([x*75,(y-1)*75])
+        elif board_representation[y-1][x].isupper() != isWhite:
+            opponents.append([x*75,(y-1)*75])
+
+    if y+1<8 and x+1<8:
+        if board_representation[y+1][x+1] == 0:
+            ValidMoves.append([(x+1)*75,(y+1)*75])
+        elif board_representation[y+1][x+1].isupper() != isWhite:
+            opponents.append([(x+1)*75,(y+1)*75])
+
+    if y+1<8 and x-1>0:
+        if board_representation[y+1][x-1] == 0:
+            ValidMoves.append([(x-1)*75,(y+1)*75])
+        elif board_representation[y+1][x-1].isupper() != isWhite:
+            opponents.append([(x-1)*75,(y+1)*75])
+
+    if y-1<8 and x+1<8:
+        if board_representation[y-1][x+1] == 0:
+            ValidMoves.append([(x+1)*75,(y-1)*75])
+        elif board_representation[y-1][x+1].isupper() != isWhite:
+            opponents.append([(x+1)*75,(y-1)*75])
+
+    if y-1<8 and x-1>0:
+        if board_representation[y-1][x-1] == 0:
+            ValidMoves.append([(x-1)*75,(y-1)*75])
+        elif board_representation[y-1][x-1].isupper() != isWhite:
+            opponents.append([(x-1)*75,(y-1)*75])
+
+    return ValidMoves,opponents
--- board.py
+++ board.py
@@ -2,83 +2,93 @@ import pygame
 
 class Board:
     def __init__(self,screen):
-        self.screen=screen
-        self.board_representation = [[0,0,0,0,0,0,0,0],
+        self.screen = screen
+        self.board_representation = [['r','n','b','k','q','b','n','r'],
+                                     ['p','p','p','p','p','p','p','p'],
                                      [0,0,0,0,0,0,0,0],
                                      [0,0,0,0,0,0,0,0],
                                      [0,0,0,0,0,0,0,0],
                                      [0,0,0,0,0,0,0,0],
-                                     [0,0,0,0,0,0,0,0],
-                                     [0,0,0,0,0,0,0,0],
-                                     [0,0,0,0,0,0,0,0]]
+                                     ['P','P','P','P','P','P','P','P'],
+                                     ['R','N','B','K','Q','B','N','R']]
         self.cordinates()
-        # print(self.cord)
 
     def drawBoard(self):
         white = (255, 255, 255)
         green = (50, 168, 82)
-        x=0
-        y=0
+        x = 0
+        y = 0
         for i in range(8):
             for j in range(8):
-                g=((i+j)%2 == 0)
+                g = ((i+j)%2 == 0)
                 squarecolor = white if g else green
                 pygame.draw.rect(self.screen, squarecolor,(x, y, 75, 75))
-                x+=75
-            x=0
-            y+=75
+                x += 75
+            x = 0
+            y += 75
     def cordinates(self):
         self.cord = {}
-        x=0
-        y=0
+        x = 0
+        y = 0
         for i in range(1,65):
-            self.cord[i]=[x,y]
-            x+=75
-            if i%8==0:
-                x=0
-                y+=75
+            self.cord[i] = [x,y]
+            x += 75
+            if i%8 == 0:
+                x = 0
+                y += 75
 
     def fen_notation(self,notation,pieceSet):
-        x=0
-        y=0
+        x = 0
+        y = 0
         for n in notation:
             try:
-                n=int(n)
+                n = int(n)
                 forward = (n-1)*75
-                x+=forward
+                x += forward
             except:
                 pass
-            if n=='k':
+            if n == 'k':
                 pieceSet.king.locations.append([x,y])
-            if n=='q':
+            if n == 'q':
                 pieceSet.queen.locations.append([x,y])
-            if n=='b':
+            if n == 'b':
                 pieceSet.bishop.locations.append([x,y])
-            if n=='n':
+            if n == 'n':
                 pieceSet.knight.locations.append([x,y])
-            if n=='r':
+            if n == 'r':
                 pieceSet.rook.locations.append([x,y])
-            if n=='p':
+            if n == 'p':
                 pieceSet.pawn.locations.append([x,y])
-            if n=='K':
+            if n == 'K':
                 pieceSet.KING.locations.append([x,y])
-            if n=='Q':
+            if n == 'Q':
                 pieceSet.QUEEN.locations.append([x,y])
-            if n=='B':
+            if n == 'B':
                 pieceSet.BISHOP.locations.append([x,y])
-            if n=='N':
+            if n == 'N':
                 pieceSet.KNIGHT.locations.append([x,y])
-            if n=='R':
+            if n == 'R':
                 pieceSet.ROOK.locations.append([x,y])
-            if n=='P':
+            if n == 'P':
                 pieceSet.PAWN.locations.append([x,y])
-            x+=75
-            if n=='/':
-                x=0
-                y+=75
+            x += 75
+            if n == '/':
+                x = 0
+                y += 75
 
-    def selected_highlight(self,cord):
-        pygame.draw.rect(self.screen, (118, 118, 26),(cord[0], cord[1], 75, 75))
+    def selected_highlight(self,cords:list):
+        s = pygame.Surface((75,75))
+        s.set_alpha(180)
+        s.fill((118, 118, 26))
+        for cord in cords:
+            self.screen.blit(s,(cord[0],cord[1]))
+            # pygame.draw.rect(self.screen, (118, 118, 26),(cord[0], cord[1], 75, 75))
+    def opponent_highlight(self,cords):
+        s = pygame.Surface((75,75))
+        # s.set_alpha(190)
+        s.fill((248, 62, 62))
+        for cord in cords:
+            self.screen.blit(s,(cord[0],cord[1]))
 
     def update_board_representation(self,list_of_pieces_objects):
         for i in range(8):
@@ -86,8 +96,6 @@ class Board:
                 self.board_representation[i][j]=0
         for piece in list_of_pieces_objects:
             for location in piece.locations:
-                if piece.name=='p':
-                    print(location)
                 x = location[0]
                 y = location[1]
                 x = int(x/75)
