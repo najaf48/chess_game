dd7b92a9a8e44beae96d726e73e08acd40a5b71a
Najaf <najaf1232075@gmail.com>
2023-08-15 14:06:41 +0530

    added GenerateMoves

---
+++ GenrateMoves.py
@@ -0,0 +1,9 @@
+from ValidMoveGenerator import GenerateMove
+
+class moves:
+
+    def __init__(self,screen,board_representation):
+        self.screen = screen
+        self.board_representation = board_representation
+        self.CurrentMoves = []
+    
